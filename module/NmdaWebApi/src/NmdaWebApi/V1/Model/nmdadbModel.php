a24de125104d8b36a4f6383e007a055530ce2c6a
Hristo Ivanov <hristo.idgh@gmail.com>
2014-09-01 13:22:24 +0200

    Channel stats query optimized. Problem: the lack of pivot and unpivot in MySql.

--- module/NmdaWebApi/src/NmdaWebApi/V1/Model/nmdadbModel.php
+++ module/NmdaWebApi/src/NmdaWebApi/V1/Model/nmdadbModel.php
@@ -47,6 +47,27 @@ class nmdadbModel {
                  select 'ch18'as channel,  ch18 as last, avg(ch18) as average, std(ch18) as std, max(ch18) as max, min(ch18) as min from(select ch18 from  binTable where start_date_time between '".$start."' and '".$finish."' order by start_date_time desc)as t18
 ;";		
 
+
+	$sql = 	"select 'ch01' as n1, ch01, avg(ch01) as avg_ch01, std(ch01) as std_ch01, max(ch01) as max_ch01, min(ch01) as min_ch01,
+			'ch02' as n2, ch02, avg(ch02) as avg_ch02, std(ch02) as std_ch02, max(ch02) as max_ch02, min(ch02) as min_ch02,
+			'ch03' as n3, ch03, avg(ch03) as avg_ch03, std(ch03) as std_ch03, max(ch03) as max_ch03, min(ch03) as min_ch03,
+			'ch04' as n4, ch04, avg(ch04) as avg_ch04, std(ch04) as std_ch04, max(ch04) as max_ch04, min(ch04) as min_ch04,
+			'ch05' as n5, ch05, avg(ch05) as avg_ch05, std(ch05) as std_ch05, max(ch05) as max_ch05, min(ch05) as min_ch05,
+			'ch06' as n6, ch06, avg(ch06) as avg_ch06, std(ch06) as std_ch06, max(ch06) as max_ch06, min(ch06) as min_ch06,
+			'ch07' as n7, ch07, avg(ch07) as avg_ch07, std(ch07) as std_ch07, max(ch07) as max_ch07, min(ch07) as min_ch07,
+			'ch08' as n8, ch08, avg(ch08) as avg_ch08, std(ch08) as std_ch08, max(ch08) as max_ch08, min(ch08) as min_ch08,
+			'ch09' as n9, ch09, avg(ch09) as avg_ch09, std(ch09) as std_ch09, max(ch09) as max_ch09, min(ch09) as min_ch09,
+			'ch10' as n10, ch10, avg(ch10) as avg_ch10, std(ch10) as std_ch10, max(ch10) as max_ch10, min(ch10) as min_ch10,
+			'ch11' as n11, ch11, avg(ch11) as avg_ch11, std(ch11) as std_ch11, max(ch11) as max_ch11, min(ch11) as min_ch11,
+			'ch12' as n12, ch12, avg(ch12) as avg_ch12, std(ch12) as std_ch12, max(ch12) as max_ch12, min(ch12) as min_ch12,
+			'ch13' as n13, ch13, avg(ch13) as avg_ch13, std(ch13) as std_ch13, max(ch13) as max_ch13, min(ch13) as min_ch13,
+			'ch14' as n14, ch14, avg(ch14) as avg_ch14, std(ch14) as std_ch14, max(ch14) as max_ch14, min(ch14) as min_ch14,
+			'ch15' as n15, ch15, avg(ch15) as avg_ch15, std(ch15) as std_ch15, max(ch15) as max_ch15, min(ch15) as min_ch15,
+			'ch16' as n16, ch16, avg(ch16) as avg_ch16, std(ch16) as std_ch16, max(ch16) as max_ch16, min(ch16) as min_ch16,
+			'ch17' as n17, ch17, avg(ch17) as avg_ch17, std(ch17) as std_ch17, max(ch17) as max_ch17, min(ch17) as min_ch17,
+			'ch18' as n18, ch18, avg(ch18) as avg_ch18, std(ch18) as std_ch18, max(ch18) as max_ch18, min(ch18) as min_ch18
+				from(select * from  binTable where start_date_time between '".$start."' and '".$finish."' order by start_date_time desc)as t1;";
+
 	$result = $this->adapter->query($sql)->execute();
 
 	$resultSet = new ResultSet;
--- module/NmdaWebApi/src/NmdaWebApi/V1/Rest/NmdadbChannelStats/NmdadbChannelStatsResource.php
+++ module/NmdaWebApi/src/NmdaWebApi/V1/Rest/NmdadbChannelStats/NmdadbChannelStatsResource.php
@@ -69,6 +69,7 @@ class NmdadbChannelStatsResource extends AbstractResourceListener
         $start=$this->getEvent()->getRouteMatch()->getParam('start');
 	$finish=$this->getEvent()->getRouteMatch()->getParam('finish');
 
+
 	if($start=='default' || $finish=='default'){
 		$data=$this->model->statsDefault();
 	}else{
@@ -80,7 +81,133 @@ class NmdadbChannelStatsResource extends AbstractResourceListener
 
 	$dataExtJs=array();	
 
-	foreach($data as $chan){
+	$the_row=$data->current();
+
+	$dataExtJs[0]['channel']=$the_row->n1;
+	$dataExtJs[1]['channel']=$the_row->n2;
+	$dataExtJs[2]['channel']=$the_row->n3;
+	$dataExtJs[3]['channel']=$the_row->n4;
+	$dataExtJs[4]['channel']=$the_row->n5;
+	$dataExtJs[5]['channel']=$the_row->n6;
+	$dataExtJs[6]['channel']=$the_row->n7;
+	$dataExtJs[7]['channel']=$the_row->n8;
+	$dataExtJs[8]['channel']=$the_row->n9;
+	$dataExtJs[9]['channel']=$the_row->n10;
+	$dataExtJs[10]['channel']=$the_row->n11;
+	$dataExtJs[11]['channel']=$the_row->n12;
+	$dataExtJs[12]['channel']=$the_row->n13;
+	$dataExtJs[13]['channel']=$the_row->n14;
+	$dataExtJs[14]['channel']=$the_row->n15;
+	$dataExtJs[15]['channel']=$the_row->n16;
+	$dataExtJs[16]['channel']=$the_row->n17;
+	$dataExtJs[17]['channel']=$the_row->n18;
+
+
+
+	$dataExtJs[0]['last']=(int)$the_row->ch01;
+	$dataExtJs[1]['last']=(int)$the_row->ch02;
+	$dataExtJs[2]['last']=(int)$the_row->ch03;
+	$dataExtJs[3]['last']=(int)$the_row->ch04;
+	$dataExtJs[4]['last']=(int)$the_row->ch05;
+	$dataExtJs[5]['last']=(int)$the_row->ch06;
+	$dataExtJs[6]['last']=(int)$the_row->ch07;
+	$dataExtJs[7]['last']=(int)$the_row->ch08;
+	$dataExtJs[8]['last']=(int)$the_row->ch09;
+	$dataExtJs[9]['last']=(int)$the_row->ch10;
+	$dataExtJs[10]['last']=(int)$the_row->ch11;
+	$dataExtJs[11]['last']=(int)$the_row->ch12;
+	$dataExtJs[12]['last']=(int)$the_row->ch13;
+	$dataExtJs[13]['last']=(int)$the_row->ch14;
+	$dataExtJs[14]['last']=(int)$the_row->ch15;
+	$dataExtJs[15]['last']=(int)$the_row->ch16;
+	$dataExtJs[16]['last']=(int)$the_row->ch17;
+	$dataExtJs[17]['last']=(int)$the_row->ch18;
+
+
+
+	$dataExtJs[0]['average']=(float)$the_row->avg_ch01;
+	$dataExtJs[1]['average']=(float)$the_row->avg_ch02;
+	$dataExtJs[2]['average']=(float)$the_row->avg_ch03;
+	$dataExtJs[3]['average']=(float)$the_row->avg_ch04;
+	$dataExtJs[4]['average']=(float)$the_row->avg_ch05;
+	$dataExtJs[5]['average']=(float)$the_row->avg_ch06;
+	$dataExtJs[6]['average']=(float)$the_row->avg_ch07;
+	$dataExtJs[7]['average']=(float)$the_row->avg_ch08;
+	$dataExtJs[8]['average']=(float)$the_row->avg_ch09;
+	$dataExtJs[9]['average']=(float)$the_row->avg_ch10;
+	$dataExtJs[10]['average']=(float)$the_row->avg_ch11;
+	$dataExtJs[11]['average']=(float)$the_row->avg_ch12;
+	$dataExtJs[12]['average']=(float)$the_row->avg_ch13;
+	$dataExtJs[13]['average']=(float)$the_row->avg_ch14;
+	$dataExtJs[14]['average']=(float)$the_row->avg_ch15;
+	$dataExtJs[15]['average']=(float)$the_row->avg_ch16;
+	$dataExtJs[16]['average']=(float)$the_row->avg_ch17;
+	$dataExtJs[17]['average']=(float)$the_row->avg_ch18;
+
+
+
+	$dataExtJs[0]['std']=(float)$the_row->std_ch01;
+	$dataExtJs[1]['std']=(float)$the_row->std_ch02;
+	$dataExtJs[2]['std']=(float)$the_row->std_ch03;
+	$dataExtJs[3]['std']=(float)$the_row->std_ch04;
+	$dataExtJs[4]['std']=(float)$the_row->std_ch05;
+	$dataExtJs[5]['std']=(float)$the_row->std_ch06;
+	$dataExtJs[6]['std']=(float)$the_row->std_ch07;
+	$dataExtJs[7]['std']=(float)$the_row->std_ch08;
+	$dataExtJs[8]['std']=(float)$the_row->std_ch09;
+	$dataExtJs[9]['std']=(float)$the_row->std_ch10;
+	$dataExtJs[10]['std']=(float)$the_row->std_ch11;
+	$dataExtJs[11]['std']=(float)$the_row->std_ch12;
+	$dataExtJs[12]['std']=(float)$the_row->std_ch13;
+	$dataExtJs[13]['std']=(float)$the_row->std_ch14;
+	$dataExtJs[14]['std']=(float)$the_row->std_ch15;
+	$dataExtJs[15]['std']=(float)$the_row->std_ch16;
+	$dataExtJs[16]['std']=(float)$the_row->std_ch17;
+	$dataExtJs[17]['std']=(float)$the_row->std_ch18;
+
+
+
+	$dataExtJs[0]['max']=(int)$the_row->max_ch01;
+	$dataExtJs[1]['max']=(int)$the_row->max_ch02;
+	$dataExtJs[2]['max']=(int)$the_row->max_ch03;
+	$dataExtJs[3]['max']=(int)$the_row->max_ch04;
+	$dataExtJs[4]['max']=(int)$the_row->max_ch05;
+	$dataExtJs[5]['max']=(int)$the_row->max_ch06;
+	$dataExtJs[6]['max']=(int)$the_row->max_ch07;
+	$dataExtJs[7]['max']=(int)$the_row->max_ch08;
+	$dataExtJs[8]['max']=(int)$the_row->max_ch09;
+	$dataExtJs[9]['max']=(int)$the_row->max_ch10;
+	$dataExtJs[10]['max']=(int)$the_row->max_ch11;
+	$dataExtJs[11]['max']=(int)$the_row->max_ch12;
+	$dataExtJs[12]['max']=(int)$the_row->max_ch13;
+	$dataExtJs[13]['max']=(int)$the_row->max_ch14;
+	$dataExtJs[14]['max']=(int)$the_row->max_ch15;
+	$dataExtJs[15]['max']=(int)$the_row->max_ch16;
+	$dataExtJs[16]['max']=(int)$the_row->max_ch17;
+	$dataExtJs[17]['max']=(int)$the_row->max_ch18;
+
+
+
+	$dataExtJs[0]['min']=(int)$the_row->min_ch01;
+	$dataExtJs[1]['min']=(int)$the_row->min_ch02;
+	$dataExtJs[2]['min']=(int)$the_row->min_ch03;
+	$dataExtJs[3]['min']=(int)$the_row->min_ch04;
+	$dataExtJs[4]['min']=(int)$the_row->min_ch05;
+	$dataExtJs[5]['min']=(int)$the_row->min_ch06;
+	$dataExtJs[6]['min']=(int)$the_row->min_ch07;
+	$dataExtJs[7]['min']=(int)$the_row->min_ch08;
+	$dataExtJs[8]['min']=(int)$the_row->min_ch09;
+	$dataExtJs[9]['min']=(int)$the_row->min_ch10;
+	$dataExtJs[10]['min']=(int)$the_row->min_ch11;
+	$dataExtJs[11]['min']=(int)$the_row->min_ch12;
+	$dataExtJs[12]['min']=(int)$the_row->min_ch13;
+	$dataExtJs[13]['min']=(int)$the_row->min_ch14;
+	$dataExtJs[14]['min']=(int)$the_row->min_ch15;
+	$dataExtJs[15]['min']=(int)$the_row->min_ch16;
+	$dataExtJs[16]['min']=(int)$the_row->min_ch17;
+	$dataExtJs[17]['min']=(int)$the_row->min_ch18;
+
+	/*foreach($data as $chan){
 		$dataExtJs[]=array(
 			'channel'=>$chan->channel,
 			'last'=>(int)$chan->last,
@@ -89,7 +216,7 @@ class NmdadbChannelStatsResource extends AbstractResourceListener
 			'max'=>(int)$chan->max,
 			'min'=>(int)$chan->min,
 		);
-	}
+	}*/
 
 	return $dataExtJs;
 
